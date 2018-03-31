cc8900175a0cee5baa4cf13ee2c435d1af70fb0f
rahiakela <rahi.akela@yahoo.com>
2018-03-31 12:14:15 +0530

    adding to repo

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('ng-bootsrap4-scss-application App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
